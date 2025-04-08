258252c0c178b8c866a77844470849222957d256
sungohki <sokim1201@naver.com>
2025-04-08 15:49:30 +0900

    feat: user 리듀서 함수 정의 및 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
 import loading from './loading';
+import user from './user';
 
-const rootReducer = combineReducers({ auth, loading });
+const rootReducer = combineReducers({ auth, loading, user });
 
 export default rootReducer;
---
+++ src/modules/user.js
@@ -0,0 +1,57 @@
+import { createAction, handleActions } from 'redux-actions';
+import { finishLoading, startLoading } from './loading';
+import { authCheck } from 'api/auth';
+
+const {
+  default: createRequestActionTypes,
+} = require('hooks/createRequestActionTypes');
+
+// action type
+const TEMP_SET_USER = 'user/TEMP_SET_USER'; // 사용자 임시 설정
+const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
+  createRequestActionTypes('user/CHECK'); // 사용자 정보 확인`
+
+// action creator
+export const tempSetUser = createAction(TEMP_SET_USER, (user) => user); // 사용자 임시 설정
+export const check = createAction(CHECK); // 사용자 정보 확인
+
+export const checkThunk = () => async (dispatch) => {
+  dispatch(startLoading(CHECK));
+  try {
+    const res = await authCheck(); // 사용자 정보 확인 API 호출
+    dispatch({ type: CHECK_SUCCESS, payload: res.data }); // 사용자 정보 확인 성공
+  } catch (error) {
+    console.error('사용자 정보 확인 실패', error);
+    dispatch({ type: CHECK_FAILURE, payload: error, error: true }); // 사용자 정보 확인 성공
+  }
+  dispatch(finishLoading(CHECK));
+};
+
+// initial state
+const initialState = {
+  user: null, // 사용자 정보
+  checkError: null, // 사용자 정보 확인 에러
+};
+
+// reducer
+const user = handleActions(
+  {
+    [TEMP_SET_USER]: (state, { payload: user }) => ({
+      ...state,
+      user,
+    }),
+    [CHECK_SUCCESS]: (state, { payload: user }) => ({
+      ...state,
+      user,
+      checkError: null,
+    }),
+    [CHECK_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      user: null,
+      checkError: error,
+    }),
+  },
+  initialState
+);
+
+export default user;
