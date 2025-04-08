353acce33f2946a1eb5ac03659e93336f6026f8e
sungohki <sokim1201@naver.com>
2025-04-08 15:50:02 +0900

    feat: 비동기api 요청 연결

--- src/containers/auth/LoginForm.jsx
+++ src/containers/auth/LoginForm.jsx
@@ -1,16 +1,22 @@
 import { AuthForm } from 'components/auth/AuthForm';
-import { changeField, initializeForm } from 'modules/auth';
+import { changeField, initializeForm, loginThunk } from 'modules/auth';
+import { checkThunk } from 'modules/user';
 import React, { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const LoginForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(
-    ({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(
+    ({ auth, user }) => ({
       form: auth.login,
+      auth: auth.auth,
+      authError: auth.authError,
+      user: user.user,
     }),
     shallowEqual
   );
+  const navigate = useNavigate();
 
   // 입력 시 store.auth.login 수정
   const handleChange = (e) => {
@@ -21,11 +27,30 @@ const LoginForm = () => {
   // 제출 시 store.auth.login 제출 및 초기화
   const handleSubmit = (e) => {
     e.preventDefault();
+    const { username, password } = form;
+    dispatch(loginThunk({ username, password }));
   };
 
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
+
+  useEffect(() => {
+    if (authError) {
+      console.error('Login failed', authError);
+      return;
+    }
+    if (auth) {
+      window.alert(auth.username + ' 로그인 성공');
+      dispatch(checkThunk());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   return (
     <AuthForm
--- src/containers/auth/RegisterForm.jsx
+++ src/containers/auth/RegisterForm.jsx
@@ -1,20 +1,22 @@
 import { AuthForm } from 'components/auth/AuthForm';
 import { changeField, initializeForm, registerThunk } from 'modules/auth';
+import { checkThunk } from 'modules/user';
 import React, { useEffect } from 'react';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(
-    ({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(
+    ({ auth, user }) => ({
       form: auth.register,
       auth: auth.auth,
       authError: auth.authError,
+      user: user.user,
     }),
     shallowEqual
   );
-  const navitgate = useNavigate();
+  const navigate = useNavigate();
 
   // 입력 시 store.auth.register 수정
   const handleChange = (e) => {
@@ -42,9 +44,16 @@ const RegisterForm = () => {
       return;
     }
     if (auth) {
-      console.log('회원가입 성공', auth);
+      window.alert('회원가입 성공', auth);
+      dispatch(checkThunk());
     }
-  }, [auth, authError, navitgate]);
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, [user, navigate]);
 
   return (
     <AuthForm
