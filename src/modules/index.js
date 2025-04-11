e1a7159656d4baeea789d500a61f73dc28983cf6
sungohki <sokim1201@naver.com>
2025-04-11 12:51:09 +0900

    feat: post 리듀서 정의 및 추가

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,7 +3,8 @@ import auth from './auth';
 import loading from './loading';
 import user from './user';
 import write from './write';
+import post from './post';
 
-const rootReducer = combineReducers({ auth, loading, user, write });
+const rootReducer = combineReducers({ auth, loading, user, write, post });
 
 export default rootReducer;
---
+++ src/modules/post.js
@@ -0,0 +1,60 @@
+import createRequestActionTypes from 'hooks/createRequestActionTypes';
+import { createAction, handleActions } from 'redux-actions';
+import { finishLoading, startLoading } from './loading';
+import { postsRead } from 'api/posts';
+
+// action type
+const [READ_POST, READ_POST_SUCCESS, READ_POST_FAILURE] =
+  createRequestActionTypes('post/READ_POST'); // 포스트 읽기
+const UNLOAD_POST = 'post/UNLOAD_POST'; // 포스트 언로드
+
+// action creator
+export const unloadPost = createAction(UNLOAD_POST); // 포스트 언로드
+
+// thunk function
+export const readPostThunk = (id) => async (dispatch) => {
+  dispatch(startLoading(READ_POST)); // 포스트 읽기 시작
+  try {
+    const res = await postsRead(id);
+    dispatch({
+      type: READ_POST_SUCCESS,
+      payload: res.data, // 읽은 포스트
+    });
+  } catch (error) {
+    console.error(error);
+    dispatch({
+      type: READ_POST_FAILURE,
+      payload: error,
+      error: true,
+    });
+  }
+  dispatch(finishLoading(READ_POST)); // 로딩 종료
+};
+
+// initial state
+const initialState = {
+  post: null,
+  postError: null, // 포스트 읽기 에러
+};
+
+// reducer
+const post = handleActions(
+  {
+    [READ_POST_SUCCESS]: (state, { payload: post }) => ({
+      ...state,
+      post: post,
+      postError: null,
+    }),
+    [READ_POST_FAILURE]: (state, { payload: error }) => ({
+      ...state,
+      post: null,
+      postError: error,
+    }),
+    [UNLOAD_POST]: () => ({
+      ...initialState,
+    }),
+  },
+  initialState
+);
+
+export default post;
